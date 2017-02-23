e488e9bfae98d8bdaf02ef8edab170ac2f4fbfe8
Ulrich Schmidt <u.schmidt@velian.de>
2017-02-23 10:55:34 +0100

    import logik gebaut

--- src/content/modules/blog2articles/controllers/Blog2ArticleController.php
+++ src/content/modules/blog2articles/controllers/Blog2ArticleController.php
@@ -17,6 +17,32 @@ class Blog2ArticleController extends Controller {
 		$currentPercent = floatval ( $_SESSION ["blog2article_step"] ) * $onefile;
 		return intval ( $currentPercent );
 	}
+	protected function _importEntry($blogData, $category = 1) {
+		try {
+			ContentFactory::loadBySystemnameAndLanguage ( $blogData->seo_shortname, $blogData->language );
+			return false;
+		} catch ( Exception $e ) {
+			$article = new Article ();
+			$article->systemname = $blogData->seo_shortname;
+			$article->language = $blogData->language;
+			$article->title = $blogData->title;
+			$article->active = $blogData->entry_enabled;
+			$article->autor = $blogData->author;
+			$article->lastchangeby = $blogData->author;
+			$article->content = $blogData->content_full;
+			$article->position = $_SESSION ["blog2article_step"] * 10;
+			$excerpt = $blogData->content_preview;
+			if (strlen ( trim ( strip_tags ( $excerpt ) ) ) <= 0) {
+				$excerpt = $article->content;
+			}
+			$article->excerpt = $excerpt;
+			$article->save ();
+			$article->created = $blogData->datum;
+			$article->lastmodified = $blogData->datum;
+			$article->save ();
+			return true;
+		}
+	}
 	public function nextStep() {
 		$sql = "SELECT * FROM {prefix}blog limit 1 OFFSET ? order by id";
 		$args = array (
@@ -25,6 +51,10 @@ class Blog2ArticleController extends Controller {
 		$query = Database::pQuery ( $sql, $args, true );
 		if (count ( $query ) > 0) {
 			// @TODO Daten importieren
+			$blogData = Database::fetchObject ( $query );
+			if ($blogData) {
+				$this->_importEntry($blogData)
+			}
 			$_SESSION ["blog2article_step"] += 1;
 		}
 		$html = Template::executeModuleTemplate ( "blog2article", "progressbar" );
