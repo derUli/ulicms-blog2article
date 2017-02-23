170d21540ed31ab2dc376e9f25d44e9ba9e43654
Ulrich Schmidt <u.schmidt@velian.de>
2017-02-23 12:08:38 +0100

    sprachdatei

---
+++ src/content/modules/blog2articles/lang/de.php
@@ -0,0 +1,2 @@
+<?php
+define ( "TRANSLATION_IMPORT_DATASET_X_FROM_Y", "Importiere Datensatz %x von %y" );
---
+++ src/content/modules/blog2articles/lang/en.php
@@ -0,0 +1,2 @@
+<?php
+define ( "TRANSLATION_IMPORT_DATASET_X_FROM_Y", "Import Dataset %x of %y" );
